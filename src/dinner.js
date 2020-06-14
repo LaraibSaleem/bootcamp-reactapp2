b6b3e8b942b1157828a91343e75b1fb6b27ae610
Laraib_Saleem <laraibsaleem98@gmail.com>
2020-06-14 17:17:12 +0500

    changes made in dinner.js to check workflow

--- src/dinner.js
+++ src/dinner.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Dinner(props){
     return(
         <div>
+            <h1> AoA! I am Laraib Saleem. </h1>
             <h1> Today we are serving {props.mainmeal}. </h1>
             <h1> Today we are serving {props.sweet} as well. </h1>
+            <h1> Today we are closed!. </h1>
         </div>
     );
 }
