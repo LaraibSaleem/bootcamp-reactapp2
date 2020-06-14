242b2b145c9586e9d9e7dde3c6be2bca88e971e4
Laraib_Saleem <laraibsaleem98@gmail.com>
2020-06-14 16:52:26 +0500

    reactapp created with parent comp passing data to the child comp.

---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dinner(props){
+    return(
+        <div>
+            <h1> Today we are serving {props.mainmeal}. </h1>
+            <h1> Today we are serving {props.sweet} as well. </h1>
+        </div>
+    );
+}
+
+export default Dinner;
